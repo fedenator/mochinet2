3832819a21726af3cb72b8b3c8420b31f932c4f8
= <=>
2018-02-03 01:49:47 -0300

    Improve GUI

--- src/main/frontend/components/CommentsTable.jsx
+++ src/main/frontend/components/CommentsTable.jsx
@@ -8,9 +8,9 @@ class CommentRow extends Component {
             <tr
                 onClick = { (event) => this.props.setid(this.props.id) }
                 style   = { {backgroundColor: this.props.color} }>
-              <th scope="row">{ this.props.id }</th>
-              <td>{ this.props.message }</td>
-              <td>{ this.props.date    }</td>
+              <td className='v-center h-center'>{ this.props.id }</td>
+              <td className='v-center'>{ this.props.message }</td>
+              <td className='v-center h-center'>{ this.props.date    }</td>
             </tr>
         );
     }
@@ -21,15 +21,15 @@ class CommentsTable extends Component {
         const generateTag = (id, setid, color, message, date) => <CommentRow id={id} setid={setid} color={color} message={message} date={date}/>
 
         let tags = [];
-        for (let i = 0; i < 100; i++) tags.push( generateTag(i, this.props.setid, '#f5c6cb', 'Markasdasdasdasdassadasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas', '1/1/2018') );
+        for (let i = 0; i < 100; i++) tags.push( generateTag(i, this.props.setid, '#f5c6cb', 'Markasdasdasdasdassad asdasdasdasdasdasdasdasdasdasdas dasdasdasdasdasdasdasdasdasdasdasdasdasdasdasas ssssssssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssssssssss ssaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaas', '1/1/2018') );
 
         return (
             <table className='CommentsTable table'>
                 <thead>
                     <tr>
-                        <th scope='col' style={{width: '60px'}}>#</th>
+                        <th scope='col' className='h-center' style={{width: '60px'}}>#</th>
                         <th scope='col' style={{width: 'auto'}}>Mensaje</th>
-                        <th scope='col' style={{width: '100px'}}>Fecha</th>
+                        <th scope='col' className='h-center' style={{width: '100px'}}>Fecha</th>
                     </tr>
                 </thead>
                 <tbody>
