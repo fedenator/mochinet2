7635715c5d0041ce3b0de4f78fffa20f97bc8765
= <=>
2018-03-24 03:12:37 -0300

    Add filtro de comentarios logicamente borrados

--- src/main/java/org/mochinet2/models/CommentsDAO.java
+++ src/main/java/org/mochinet2/models/CommentsDAO.java
@@ -2,9 +2,13 @@ package org.mochinet2.models;
 
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+
+import java.util.List;
 
 /**
  * Auto implemented by {@link Hibernate}
@@ -13,4 +17,6 @@ import javax.transaction.Transactional;
 @Transactional
 public interface CommentsDAO extends CrudRepository<Comment, Long> {
 
+    @Query("SELECT c FROM Comment c WHERE c.deleted = false")
+    public List<Comment> activeComments();
 }
